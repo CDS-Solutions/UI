c15459d5707c27e83997362f59162c01b6071e51
Danila Tezin <danilatezin@gmail.com>
2022-09-13 17:10:39 +0500

    add Grid component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from './App.module.css';
 import Button from './components/Button/Button';
 import CheckBox from './components/CheckBox/CheckBox';
 import ColorGroup from './components/ColorGroup/ColorGroup';
+import Grid from './components/Grid/Grid';
 import Input from './components/Input/Input';
 import Switch from './components/Switch/Switch';
 // import TransferList from './components/TransferList/TransferList';
@@ -11,6 +12,8 @@ function App() {
 
   const [disabled, setDisabled] = useState(true)
   const [buttonValue, setButtonValue] = useState<string>('ебать')
+  const [countColumns, setCountColumns] = useState<number>(2)
+  
   // const secondArray  = ['первый элемент', 'ваыаыва','ываывацуауа']
   // const firstArray  = ['первый элемент', 'ваыаыва','ываывацуауа']
 
@@ -18,39 +21,67 @@ function App() {
   return (
     <div className={styles.container}>
 
-      <ColorGroup color='#2679ff'>
+       <Grid tcolumns={3}>
+          <Button 
+            disabled={disabled}
+            varianted="outlined"
+            onClick={()=> setCountColumns(1)}
+          >
+            1
+          </Button>
+          <Button 
+            disabled={disabled}
+            varianted="outlined"
+            onClick={()=> setCountColumns(2)}
+          >
+            2
+          </Button>
+          <Button 
+            disabled={disabled}
+            varianted="outlined"
+            onClick={()=> setCountColumns(3)}
+          >
+            3
+          </Button>
+       </Grid>
 
-        <Button 
-          disabled={disabled}
-          varianted="contained"
-          onClick={() => console.log(1)}
-        >
-          {buttonValue}
-        </Button>
+      <Grid shadow='m' tcolumns={countColumns}>
+        <ColorGroup color='#2679ff'>
 
-        <Button 
-          disabled={disabled}
-          varianted="outlined"
-          onClick={() => console.log(1)}
-        >
-          {buttonValue}
-        </Button>
+  <Button 
+    disabled={disabled}
+    varianted="contained"
+    onClick={() => console.log(1)}
+  >
+    {buttonValue}
+  </Button>
 
-        <Button 
-          disabled={disabled}
-          varianted="text"
-          onClick={() => console.log(1)}
-        >
-          {buttonValue}
-        </Button>
+  <Button 
+    disabled={disabled}
+    varianted="outlined"
+    onClick={() => console.log(1)}
+  >
+    {buttonValue}
+  </Button>
 
-        <Input disabled={disabled} onChange={(e:any) => setButtonValue(e.target.value)}/>
+  <Button 
+    disabled={disabled}
+    varianted="text"
+    onClick={() => console.log(1)}
+  >
+    {buttonValue}
+  </Button>
 
-        <Switch onChange={setDisabled}/>
+  <Input disabled={disabled} onChange={(e:any) => setButtonValue(e.target.value)}/>
 
-        {/* <TransferList firstArray={firstArray} secondArray={secondArray}/> */}
-        <CheckBox label={buttonValue} disabled={disabled}/>
-      </ColorGroup>
+  <Switch onChange={setDisabled}/>
+
+  {/* <TransferList firstArray={firstArray} secondArray={secondArray}/> */}
+  <CheckBox label={buttonValue} disabled={disabled}/>
+        </ColorGroup>
+      </Grid>
+
+      
       
 
     </div>
